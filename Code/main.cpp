d1fa495b94af9242ac9d2c5b1ec709d5996c4d03
HowardYao730206 <haoyao325344@gmail.com>
2026-08-18 02:29:49 -0700

    Add Slang shader compilation to build, test triangle shader, Vulkan instance creation in main

--- Code/main.cpp
+++ Code/main.cpp
@@ -7,24 +7,27 @@
 
 int main()
 {
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	if (!glfwInit())
+	// Test glm
+	glm::vec3 testVec(1.0f, 2.0f, 3.0f);
+	std::printf("Test glm::vec3: (%f, %f, %f)\n", testVec.x, testVec.y, testVec.z);
+
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.apiVersion = VK_API_VERSION_1_4;
+	appInfo.pApplicationName = "Vulkan App";
+
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
+	VkInstance instance;
+	VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+	if (result != VK_SUCCESS)
 	{
-		std::cerr << "Failed to initialize GLFW" << std::endl;
+		std::cerr << "Failed to create Vulkan instance: " << result << std::endl;
 		return -1;
 	}
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
 
-	if (!window)
-	{
-		std::cerr << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-
-	while (!glfwWindowShouldClose(window))
-	{
-		glfwPollEvents();
-	}
+	std::cout << "Vulkan instance created successfully." << std::endl;
 	return 0;
 }
